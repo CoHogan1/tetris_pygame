c4140df3968ad622d19d2e99f8ec204705f4f318
Conner Hogan <ConnerRHogan@Gmail.com>
2024-03-13 22:10:39 -0600

    added game board, and fill functionality. fill 2d list off numbers

--- main.py
+++ main.py
@@ -3,24 +3,29 @@ from pygame import mixer
 from pygame.locals import*
 import random
 
+from grid import Grid
+
 pygame.init()
 
 # set up game screen
-screen = pygame.display.set_mode((300,600))
+# screen = pygame.display.set_mode((300,600))
+screen = pygame.display.set_mode((500,800))
 pygame.display.set_caption("Pygame Tetris")
 fps = 60
 clock = pygame.time.Clock()
 
 dark_blue = (44, 44, 127)
 
-
-
-
 print('running file')
 
 
 
+game_grid = Grid()
+#game_grid.print_grid()
 
+game_grid.grid[0][0] = 1  3 32:13
+game_grid.grid[3][5] = 4
+game_grid.grid[17][8] = 7
 
 run = True
 while run:
@@ -32,6 +37,10 @@ while run:
             run = False
 
     screen.fill(dark_blue)
+    game_grid.draw(screen)
+
+
+
     pygame.display.update()
 
 
